14c37db8d4f97f24d335e31df93a3ed0644dbca0
unknown <macjay314@gmail.com>
2020-05-11 10:35:01 -0700

    Create first test 'e' -> '3'

---
+++ Leetspeak.Tests/ModelTests/LeetspeakTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leet;
+
+namespace Leet.Tests
+{
+  [TestClass]
+  public class LeetspeakTests
+  {
+    [TestMethod]
+    public void Replaceecharwith3()
+    {
+      LeetspeakTranslator testTranslation = new LeetspeakTranslator();
+      Assert.AreEqual(l33t, testTranslation.Translate(leet));
+    }
+  }
+}
