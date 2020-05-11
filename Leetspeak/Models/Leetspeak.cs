3a9984a3f6754c90516359c00dcbad6a9a559335
unknown <macjay314@gmail.com>
2020-05-11 11:02:08 -0700

    Add first test functionality

--- Leetspeak/Models/Leetspeak.cs
+++ Leetspeak/Models/Leetspeak.cs
@@ -5,6 +5,15 @@ namespace Leet
     public string Translate(string input)
     {
       char[] array = input.ToCharArray();
+      for (int i = 0; i < array.Length; i++)
+      {
+        if (array[i] == 'e')
+        {
+          array[i] = '3';
+        }
+      }
+      string result = string.Join("", array);
+      return result;
     }
   }
 }
